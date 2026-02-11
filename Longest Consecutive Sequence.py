f3b0ca28f87f41b2e1b72d65d9c80bad619f2a23
Feven Tassew <feventassew1997@gmail.com>
2026-02-11 06:43:02 +0300

    Create  Longest Consecutive Sequence.py

---
+++ Longest Consecutive Sequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        if not nums:
+            return 0
+
+        nums2 = set(nums)      # remove duplicates
+        nums3 = list(nums2)
+        nums3.sort()
+
+        ans = 1
+        count = 1
+
+        for i in range(1, len(nums3)):
+            if nums3[i] == nums3[i-1] + 1:
+                count += 1
+            else:
+                count = 1
+
+            ans = max(ans, count)
+
+        return ans
